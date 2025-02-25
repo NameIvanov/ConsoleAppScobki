e1676a99e44cd109e727a0e6ff63bf9fd28a34b0
NameIvanov <toster.one@yandex.ru>
2025-02-25 11:16:46 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppScobki/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppScobki
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stroka = Console.ReadLine();
+            Stack<string> itog = new Stack<string> { };
+            if (stroka != null)
+            {
+                foreach (char ch in stroka)
+                {
+                    if (ch == '(')
+                        itog.Push(Convert.ToString(ch));
+                    else if (ch == ')')
+                    {
+                        if (itog.Count == 0)
+                        {
+                            Console.WriteLine("NO");
+                            goto use;
+                        }
+                        itog.Pop();
+                    }
+                }
+                if (itog.Count == 0)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+                use:;
+            }
+            else
+                Console.WriteLine("YES");
+        }
+    }
+}
